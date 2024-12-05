230fab337414c1a969c60469903d8f7b406457be
Aftaab Siddiqui <aftaab2507@gmail.com>
2024-12-05 23:47:16 +0530

    Add day5 part1 solution

---
+++ day-5/part1/part1.go
@@ -0,0 +1,81 @@
+package part1
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func getFileData() (*os.File, *bufio.Scanner) {
+	wd, err := os.Getwd()
+	check(err)
+
+	inputPath := filepath.Join(wd, "input.txt")
+
+	file, err := os.Open(inputPath)
+	check(err)
+
+	return file, bufio.NewScanner(file)
+}
+
+func checkUpdatesOrder(rules []string, updates []string) bool {
+	for i, _ := range updates {
+		ele := updates[i]
+		for j := i + 1; j < len(updates); j++ {
+			check := updates[j] + "|" + string(ele)
+			if slices.Contains(rules, check) {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func Run() {
+
+	file, scanner := getFileData()
+	defer file.Close()
+
+	ans := 0
+	rules := []string{}
+	updates := []string{}
+	flag := false
+	for scanner.Scan() {
+		s := scanner.Text()
+		line := strings.Fields(s)
+		// fmt.Println(line)
+
+		if len(line) == 0 {
+			flag = true
+			continue
+		}
+
+		if flag {
+			updates = append(updates, line[0])
+		} else {
+			rules = append(rules, line[0])
+		}
+	}
+
+	for _, v := range updates {
+		updates := strings.Split(v, ",")
+		if checkUpdatesOrder(rules, updates) {
+			n := len(updates) / 2
+			x, _ := strconv.Atoi(updates[n])
+			ans += x
+		}
+	}
+
+	fmt.Println(ans)
+}
